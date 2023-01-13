4804117546decb4ea05d74e45fde2a1c633c965c
stassie-lzrv <adlazareva@edu.hse.ru>
2023-01-13 14:51:24 +0300

    Initial Commit

---
+++ EducationalAppForChildren/EducationalAppForChildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationalAppForChildrenApp.swift
+//  EducationalAppForChildren
+//
+//  Created by Настя Лазарева on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EducationalAppForChildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
